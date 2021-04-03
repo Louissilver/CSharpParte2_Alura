09f2b66ea36cb88658c737d0cc2bf0ee695947a3
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-04-03 11:23:00 -0300

    Ajustes nos projetos e primeiro método

--- 01-ByteBank/ContaCorrente.cs
+++ 01-ByteBank/ContaCorrente.cs
@@ -3,5 +3,20 @@
     public string titular;
     public int agencia;
     public int numero;
-    public double saldo;
+    public double saldo = 100;
+
+    /// <summary>Método para realizar saque na conta.
+    /// Métodos, por convenção, sempre são escritos no infinitivo e iniciam com letra maiúscula
+    /// <para><see valor/> Valor a ser sacado.</para>
+    /// </summary>
+    public bool Sacar(double valor)
+    {
+        bool ehPossivelSacar = true;
+        if (this.saldo > valor)
+        {
+            this.saldo -= valor;
+            ehPossivelSacar = true;
+        }
+        return ehPossivelSacar;
+    }
 }
--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,14 +10,25 @@ namespace _01_ByteBank
             contaCorrente.titular = "Gabriela";
             contaCorrente.agencia = 863;
             contaCorrente.numero = 15;
-            contaCorrente.saldo = 537.76;
+            contaCorrente.saldo = 300;
 
-            Console.WriteLine(contaCorrente.titular);
-            Console.WriteLine(contaCorrente.agencia);
-            Console.WriteLine(contaCorrente.numero);
-            Console.WriteLine(contaCorrente.saldo);
+            Console.WriteLine("Conta corrente da Gabriela e valores dos atributos:");
+            Console.WriteLine("Titular: " + contaCorrente.titular);
+            Console.WriteLine("Agência: " + contaCorrente.agencia);
+            Console.WriteLine("Número: " + contaCorrente.numero);
+            Console.WriteLine("Saldo: " + contaCorrente.saldo + "\n\n");
 
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine("Conta corrente do Bruno com exibição de saldo antes e depois do saque:");
+            Console.WriteLine(contaDoBruno.titular);
+            Console.WriteLine("Saldo anterior: " + contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+            Console.WriteLine("Resultado do saque: " + resultadoSaque);
+            Console.WriteLine("Saldo posterior: " + contaDoBruno.saldo);
             Console.ReadLine();
+
         }
     }
 }
